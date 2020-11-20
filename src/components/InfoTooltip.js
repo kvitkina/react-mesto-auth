802f69d4451b325617a10c7c2a7efda4eb360f5a
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-11-20 22:36:24 +0300

    infoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -2,18 +2,21 @@ import React from 'react';
 import success from '../images/success.svg'
 import fail from '../images/fail.svg'
 
-const InfoTooltip = (name, onClose, isOpen, title) => {
+const InfoTooltip = ({ tooltipStatus, onClose, isOpen }) => {
   return (
     <section
-      className='popup '
-    //   className={`popup popup_${name} ${isOpen && `popup_opened`} `}
+      className={`popup ${isOpen && `popup_opened`} `}
       onClick={(evt) => (evt.target !== evt.currentTarget) && onClose }
     >
       <div className="popup__container">
         <button className="popup__close" onClick={onClose} />
-        <img className="popup__icon" src={success} alt="Tooltip"/>
+        <img 
+          className="popup__icon" 
+          src={tooltipStatus ? success : fail }
+          alt="Tooltip"
+        />
         <h2 className="popup__title popup__title_theme_tooltip">
-          Вы успешно зарегистрировались!
+          {tooltipStatus ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так! Попробуйте ещё раз.'}
         </h2>
       </div>
     </section>
